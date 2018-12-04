223fa4f05c4b8276bf90a68715595524cd716de6
bvermass <basilevermassen@hotmail.com>
2018-12-04 18:25:39 +0100

    hadd several backgrounds together into general backgrounds like ttbar and DY

--- test/scripts/hadd_subfiles.sh
+++ test/scripts/hadd_subfiles.sh
@@ -17,3 +17,13 @@ function hadd_all {
 for D in *; do
     hadd_all $D 
 done
+
+#Now do specific hadding of different background files:
+cd /user/bvermass/public/2l2q_analysis/histograms/full_analyzer/
+#ttbar
+hadd -f hists_full_analyzer_Background_TTJets.root hists_full_analyzer_Background_TTJets_DiLept_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root hists_full_analyzer_Background_TTJets_SingleLeptFromT_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root hists_full_analyzer_Background_TTJets_SingleLeptFromTbar_TuneCUETP8M1_13TeV-madgraphMLM-pythia8.root
+#DY
+hadd -f hists_full_analyzer_Background_DYJets.root hists_full_analyzer_Background_DYJetsToLL_M-10to50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8.root hists_full_analyzer_Background_DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8_ext1-v2.root
+#diboson - ADD OTHERS WHEN THEY ARE WORKING
+hadd -f hists_full_analyzer_Background_Diboson.root hists_full_analyzer_Background_WGToLNuG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1-v1.root hists_full_analyzer_Background_ZGTo2LG_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_ext1-v1.root
+
