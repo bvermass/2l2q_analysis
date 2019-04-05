d2b951ea4503b4e012d30c54e0760d29d0dbca74
bvermass <basilevermassen@hotmail.com>
2019-04-05 16:26:31 +0200

    Rewrite analyze_cuts to check IVF efficiency improvement

--- src/analyze_cuts.cc
+++ src/analyze_cuts.cc
@@ -23,107 +23,53 @@
 
 using namespace std;
 
+std::vector<TString> get_signs_and_flavor(int argc, char * argv[]){
+    bool e = false, mu = false;
+    for(int i = 1; i < (argc + 1)/2; i++){
+        TString name = (TString)argv[i];
+        if(name.Index("_e_") != -1) e = true;
+        if(name.Index("_mu_") != -1) mu = true;
+    }
+
+    if(!e and !mu){ e = true; mu = true;}               // if neither are true, there are only background samples and you would want both e and mu
+
+    // fill vector based on flavors wanted
+    std::vector<TString> signs_and_flavor_vec;
+    if(mu){
+        signs_and_flavor_vec.push_back("_OS_mu");
+        signs_and_flavor_vec.push_back("_SS_mu");
+    }
+    if(e){
+        signs_and_flavor_vec.push_back("_OS_e");
+        signs_and_flavor_vec.push_back("_SS_e");
+    }
+
+    return signs_and_flavor_vec;
+}
+
+
 # ifndef __CINT__
 int main(int argc, char * argv[])
 {
-    TString pathname = "/user/bvermass/public/2l2q_analysis/log/";
-    //This file contains everything regarding the optimization of cuts
-    //Essential information is number of events left after a cut.
-    //This can be put in several different histograms or in one histogram altogether
-    //several histograms is more overseeable, while one histogram with bins is simpler to implement but less clear
-    //manipulations: sums of histograms, divisions, ... lets start with one histogram and see how that works
-
-    //for now: sum all background, 
-    std::map<TString, TFile*> files;
-    for(int i = 1; i < (argc + 1)/2; i++){
-        files[argv[i]] = TFile::Open(argv[i]);
-        cout << argv[i] << endl;
+    // Open files, get wanted histograms and print latex table in the same loop since I don't need any other functionality
+    std::vector<TString> signs_and_flavor_vec = get_signs_and_flavor(argc, argv);
+    double accuracy = 0.1;
+    for(TString sf_it : signs_and_flavor_vec){
+        cout << "----------" << sf_it << "-----------" << endl;
+        cout << "\\scalebox{0.85}{" << endl;
+        cout << "\\begin{tabular}{|c|c|c|}" << endl;
+        cout << "\\Process & $l_2$ Vtx & SVgenreco < 0.2cm \\\\" << endl;
+        cout << "\\hline" << endl;
+        for(int i = 1; i < (argc + 1)/2; i++){
+            TString name = (TString)argv[(argc - 1)/2 + i];
+            TFile*  file = TFile::Open(argv[i]);
+            TH1F* hist = (TH1F*) file->Get(sf_it+"_IVF_categories");
+            cout << hist->GetXaxis()->GetBinLabel(1) << " " << hist->GetXaxis()->GetBinLabel(2) << endl;
+            cout << name << " & " << accuracy*round(hist->GetBinContent(2)/hist->GetBinContent(1)/accuracy) << " & " << accuracy*round(hist->GetBinContent(3)/hist->GetBinContent(1)/accuracy) << " \\\\" << endl;
+        }
+        cout << "\\hline" << endl;
+        cout << "\\end{tabular}" << endl;
+        cout << "}" << endl;
     }
-    
-    TFile * HNL3GeV_e_file = TFile::Open("/user/bvermass/public/2l2q_analysis/histograms/full_analyzer/hists_full_analyzer_HeavyNeutrino_lljj_M-3_V-0.00836660026534_e_onshell_pre2017_leptonFirst_NLO_displaced.root");
-    TFile * HNL3GeV_mu_file = TFile::Open("/user/bvermass/public/2l2q_analysis/histograms/full_analyzer/hists_full_analyzer_HeavyNeutrino_lljj_M-3_V-0.00836660026534_mu_onshell_pre2017_leptonFirst_NLO_displaced.root");
-    TFile * HNL7GeV_e_file = TFile::Open("/user/bvermass/public/2l2q_analysis/histograms/full_analyzer/hists_full_analyzer_HeavyNeutrino_lljj_M-7_V-0.00244948974278_e_onshell_pre2017_leptonFirst_NLO_displaced.root");
-    TFile * HNL7GeV_mu_file = TFile::Open("/user/bvermass/public/2l2q_analysis/histograms/full_analyzer/hists_full_analyzer_HeavyNeutrino_lljj_M-7_V-0.00244948974278_mu_onshell_pre2017_leptonFirst_NLO_displaced.root");
-    TFile * HNL10GeV_e_file = TFile::Open("/user/bvermass/public/2l2q_analysis/histograms/full_analyzer/hists_full_analyzer_HeavyNeutrino_lljj_M-10_V-0.00244948974278_e_onshell_pre2017_leptonFirst_NLO_displaced.root");
-    TFile * HNL10GeV_mu_file = TFile::Open("/user/bvermass/public/2l2q_analysis/histograms/full_analyzer/hists_full_analyzer_HeavyNeutrino_lljj_M-10_V-0.00244948974278_mu_onshell_pre2017_leptonFirst_NLO_displaced.root");
-
-    std::map<TString, TH1*> hists;
-
-    //sum all background together for SS-OS, e-mu
-    hists["bkg_displ_SS_mu_cuts"] = (TH1F*) files[argv[1]]->Get("displ_SS_mu_cuts");
-    hists["bkg_displ_OS_mu_cuts"] = (TH1F*) files[argv[1]]->Get("displ_OS_mu_cuts");
-    hists["bkg_displ_SS_e_cuts"] = (TH1F*) files[argv[1]]->Get("displ_SS_e_cuts");
-    hists["bkg_displ_OS_e_cuts"] = (TH1F*) files[argv[1]]->Get("displ_OS_e_cuts");
-    for(int i = 2; i < (argc + 1)/2; i++){
-        hists["bkg_displ_SS_mu_cuts"]->Add((TH1F*)files[argv[i]]->Get("displ_SS_mu_cuts"));
-        hists["bkg_displ_OS_mu_cuts"]->Add((TH1F*)files[argv[i]]->Get("displ_OS_mu_cuts"));
-        hists["bkg_displ_SS_e_cuts"]->Add((TH1F*)files[argv[i]]->Get("displ_SS_e_cuts"));
-        hists["bkg_displ_OS_e_cuts"]->Add((TH1F*)files[argv[i]]->Get("displ_OS_e_cuts"));
-    }
-
-    //get signal histograms
-    hists["3GeV_displ_SS_mu_cuts"] = (TH1F*) HNL3GeV_mu_file->Get("displ_SS_mu_cuts");
-    hists["3GeV_displ_OS_mu_cuts"] = (TH1F*) HNL3GeV_mu_file->Get("displ_OS_mu_cuts");
-    hists["3GeV_displ_SS_e_cuts"] = (TH1F*) HNL3GeV_e_file->Get("displ_SS_e_cuts");
-    hists["3GeV_displ_OS_e_cuts"] = (TH1F*) HNL3GeV_e_file->Get("displ_OS_e_cuts");
-    hists["7GeV_displ_SS_mu_cuts"] = (TH1F*) HNL7GeV_mu_file->Get("displ_SS_mu_cuts");
-    hists["7GeV_displ_OS_mu_cuts"] = (TH1F*) HNL7GeV_mu_file->Get("displ_OS_mu_cuts");
-    hists["7GeV_displ_SS_e_cuts"] = (TH1F*) HNL7GeV_e_file->Get("displ_SS_e_cuts");
-    hists["7GeV_displ_OS_e_cuts"] = (TH1F*) HNL7GeV_e_file->Get("displ_OS_e_cuts");
-    hists["10GeV_displ_SS_mu_cuts"] = (TH1F*) HNL10GeV_mu_file->Get("displ_SS_mu_cuts");
-    hists["10GeV_displ_OS_mu_cuts"] = (TH1F*) HNL10GeV_mu_file->Get("displ_OS_mu_cuts");
-    hists["10GeV_displ_SS_e_cuts"] = (TH1F*) HNL10GeV_e_file->Get("displ_SS_e_cuts");
-    hists["10GeV_displ_OS_e_cuts"] = (TH1F*) HNL10GeV_e_file->Get("displ_OS_e_cuts");
-
-    //now get the efficiencies, dividing bins 8 and 9 by bin 7
-    double _bkg_displ_SS_mu_oldID_eff = hists["bkg_displ_SS_mu_cuts"]->GetBinContent(4) / hists["bkg_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _bkg_displ_OS_mu_oldID_eff = hists["bkg_displ_OS_mu_cuts"]->GetBinContent(4) / hists["bkg_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _bkg_displ_SS_e_oldID_eff = hists["bkg_displ_SS_e_cuts"]->GetBinContent(4) / hists["bkg_displ_SS_e_cuts"]->GetBinContent(3);
-    double _bkg_displ_OS_e_oldID_eff = hists["bkg_displ_OS_e_cuts"]->GetBinContent(4) / hists["bkg_displ_OS_e_cuts"]->GetBinContent(3);
-    double _3GeV_displ_SS_mu_oldID_eff = hists["3GeV_displ_SS_mu_cuts"]->GetBinContent(4) / hists["3GeV_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _3GeV_displ_OS_mu_oldID_eff = hists["3GeV_displ_OS_mu_cuts"]->GetBinContent(4) / hists["3GeV_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _3GeV_displ_SS_e_oldID_eff = hists["3GeV_displ_SS_e_cuts"]->GetBinContent(4) / hists["3GeV_displ_SS_e_cuts"]->GetBinContent(3);
-    double _3GeV_displ_OS_e_oldID_eff = hists["3GeV_displ_OS_e_cuts"]->GetBinContent(4) / hists["3GeV_displ_OS_e_cuts"]->GetBinContent(3);
-    double _7GeV_displ_SS_mu_oldID_eff = hists["7GeV_displ_SS_mu_cuts"]->GetBinContent(4) / hists["7GeV_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _7GeV_displ_OS_mu_oldID_eff = hists["7GeV_displ_OS_mu_cuts"]->GetBinContent(4) / hists["7GeV_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _7GeV_displ_SS_e_oldID_eff = hists["7GeV_displ_SS_e_cuts"]->GetBinContent(4) / hists["7GeV_displ_SS_e_cuts"]->GetBinContent(3);
-    double _7GeV_displ_OS_e_oldID_eff = hists["7GeV_displ_OS_e_cuts"]->GetBinContent(4) / hists["7GeV_displ_OS_e_cuts"]->GetBinContent(3);
-    double _10GeV_displ_SS_mu_oldID_eff = hists["10GeV_displ_SS_mu_cuts"]->GetBinContent(4) / hists["10GeV_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _10GeV_displ_OS_mu_oldID_eff = hists["10GeV_displ_OS_mu_cuts"]->GetBinContent(4) / hists["10GeV_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _10GeV_displ_SS_e_oldID_eff = hists["10GeV_displ_SS_e_cuts"]->GetBinContent(4) / hists["10GeV_displ_SS_e_cuts"]->GetBinContent(3);
-    double _10GeV_displ_OS_e_oldID_eff = hists["10GeV_displ_OS_e_cuts"]->GetBinContent(4) / hists["10GeV_displ_OS_e_cuts"]->GetBinContent(3);
-
-    double _bkg_displ_SS_mu_newID_eff = hists["bkg_displ_SS_mu_cuts"]->GetBinContent(5) / hists["bkg_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _bkg_displ_OS_mu_newID_eff = hists["bkg_displ_OS_mu_cuts"]->GetBinContent(5) / hists["bkg_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _bkg_displ_SS_e_newID_eff = hists["bkg_displ_SS_e_cuts"]->GetBinContent(5) / hists["bkg_displ_SS_e_cuts"]->GetBinContent(3);
-    double _bkg_displ_OS_e_newID_eff = hists["bkg_displ_OS_e_cuts"]->GetBinContent(5) / hists["bkg_displ_OS_e_cuts"]->GetBinContent(3);
-    double _3GeV_displ_SS_mu_newID_eff = hists["3GeV_displ_SS_mu_cuts"]->GetBinContent(5) / hists["3GeV_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _3GeV_displ_OS_mu_newID_eff = hists["3GeV_displ_OS_mu_cuts"]->GetBinContent(5) / hists["3GeV_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _3GeV_displ_SS_e_newID_eff = hists["3GeV_displ_SS_e_cuts"]->GetBinContent(5) / hists["3GeV_displ_SS_e_cuts"]->GetBinContent(3);
-    double _3GeV_displ_OS_e_newID_eff = hists["3GeV_displ_OS_e_cuts"]->GetBinContent(5) / hists["3GeV_displ_OS_e_cuts"]->GetBinContent(3);
-    double _7GeV_displ_SS_mu_newID_eff = hists["7GeV_displ_SS_mu_cuts"]->GetBinContent(5) / hists["7GeV_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _7GeV_displ_OS_mu_newID_eff = hists["7GeV_displ_OS_mu_cuts"]->GetBinContent(5) / hists["7GeV_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _7GeV_displ_SS_e_newID_eff = hists["7GeV_displ_SS_e_cuts"]->GetBinContent(5) / hists["7GeV_displ_SS_e_cuts"]->GetBinContent(3);
-    double _7GeV_displ_OS_e_newID_eff = hists["7GeV_displ_OS_e_cuts"]->GetBinContent(5) / hists["7GeV_displ_OS_e_cuts"]->GetBinContent(3);
-    double _10GeV_displ_SS_mu_newID_eff = hists["10GeV_displ_SS_mu_cuts"]->GetBinContent(5) / hists["10GeV_displ_SS_mu_cuts"]->GetBinContent(3);
-    double _10GeV_displ_OS_mu_newID_eff = hists["10GeV_displ_OS_mu_cuts"]->GetBinContent(5) / hists["10GeV_displ_OS_mu_cuts"]->GetBinContent(3);
-    double _10GeV_displ_SS_e_newID_eff = hists["10GeV_displ_SS_e_cuts"]->GetBinContent(5) / hists["10GeV_displ_SS_e_cuts"]->GetBinContent(3);
-    double _10GeV_displ_OS_e_newID_eff = hists["10GeV_displ_OS_e_cuts"]->GetBinContent(5) / hists["10GeV_displ_OS_e_cuts"]->GetBinContent(3);
-
-    
-    cout << endl << endl << "SS mu" << endl;
-    cout << "oldID bkg, 3, 7, 10 : " << _bkg_displ_SS_mu_oldID_eff << " " << _3GeV_displ_SS_mu_oldID_eff << " " << _7GeV_displ_SS_mu_oldID_eff << " " << _10GeV_displ_SS_mu_oldID_eff << endl;
-    cout << "newID bkg, 3, 7, 10 : " << _bkg_displ_SS_mu_newID_eff << " " << _3GeV_displ_SS_mu_newID_eff << " " << _7GeV_displ_SS_mu_newID_eff << " " << _10GeV_displ_SS_mu_newID_eff << endl;
-    cout << endl << endl << "OS mu" << endl;
-    cout << "oldID bkg, 3, 7, 10 : " << _bkg_displ_OS_mu_oldID_eff << " " << _3GeV_displ_OS_mu_oldID_eff << " " << _7GeV_displ_OS_mu_oldID_eff << " " << _10GeV_displ_OS_mu_oldID_eff << endl;
-    cout << "newID bkg, 3, 7, 10 : " << _bkg_displ_OS_mu_newID_eff << " " << _3GeV_displ_OS_mu_newID_eff << " " << _7GeV_displ_OS_mu_newID_eff << " " << _10GeV_displ_OS_mu_newID_eff << endl;
-    cout << endl << endl << "SS e" << endl;
-    cout << "oldID bkg, 3, 7, 10 : " << _bkg_displ_SS_e_oldID_eff << " " << _3GeV_displ_SS_e_oldID_eff << " " << _7GeV_displ_SS_e_oldID_eff << " " << _10GeV_displ_SS_e_oldID_eff << endl;
-    cout << "newID bkg, 3, 7, 10 : " << _bkg_displ_SS_e_newID_eff << " " << _3GeV_displ_SS_e_newID_eff << " " << _7GeV_displ_SS_e_newID_eff << " " << _10GeV_displ_SS_e_newID_eff << endl;
-    cout << endl << endl << "OS e" << endl;
-    cout << "oldID bkg, 3, 7, 10 : " << _bkg_displ_OS_e_oldID_eff << " " << _3GeV_displ_OS_e_oldID_eff << " " << _7GeV_displ_OS_e_oldID_eff << " " << _10GeV_displ_OS_e_oldID_eff << endl;
-    cout << "newID bkg, 3, 7, 10 : " << _bkg_displ_OS_e_newID_eff << " " << _3GeV_displ_OS_e_newID_eff << " " << _7GeV_displ_OS_e_newID_eff << " " << _10GeV_displ_OS_e_newID_eff << endl;
-
-    return 0;
 }
 # endif
