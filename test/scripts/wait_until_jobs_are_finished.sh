b16c232ee88e450dfc66ff848ebeb734359704e6
bvermass <basilevermassen@hotmail.com>
2019-04-16 21:49:25 +0200

    improve wait_until_jobs_are_finished to display amount of jobs still running

--- test/scripts/wait_until_jobs_are_finished.sh
+++ test/scripts/wait_until_jobs_are_finished.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 qstatoutput="qstatoutput.txt"
 jobsrunning=true
+njobs=0
+nrunning=0
+echo -e "\n"
 while $jobsrunning; do
     while ! qstat -u bvermass > $qstatoutput; do
         echo -e "caught qstat error, retrying"
         sleep 2
     done
-    if cat "$qstatoutput" | grep -q "bvermass"; then
-        echo -e "jobs running"
-        sleep 60 #wait a minute before trying again
-    else 
-        echo -e "jobs finished\n"
+    if [ $njobs -eq 0 ]; then
+        njobs=$(grep -c "bvermass" "$qstatoutput")
+    fi
+    nrunning=$(grep -c "bvermass" "$qstatoutput")
+    echo -ne "running/total = $nrunning/$njobs"
+    sleep 60 #wait a minute before trying again
+    if [ $nrunning -eq 0 ]; then
+        echo -e "\njobs finished\n"
         jobsrunning=false
     fi
 done
