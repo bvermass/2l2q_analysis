e7094229bbe23de8c800f535e92cb75b13aba576
bvermass <basilevermassen@hotmail.com>
2019-04-26 11:13:46 +0200

    improve wait_until_jobs_are_finished by only looking at LocalJob named jobs and let the output message give the number of jobs still running information

--- test/scripts/wait_until_jobs_are_finished.sh
+++ test/scripts/wait_until_jobs_are_finished.sh
@@ -10,14 +10,15 @@ while $jobsrunning; do
         sleep 2
     done
     if [ $njobs -eq 0 ]; then
-        njobs=$(grep -c "bvermass" "$qstatoutput")
+        njobs=$(grep -c "LocalJob" "$qstatoutput")
     fi
-    nrunning=$(grep -c "bvermass" "$qstatoutput")
-    echo -ne "running/total = $nrunning/$njobs"
-    sleep 60 #wait a minute before trying again
+    nrunning=$(grep -c "LocalJob" "$qstatoutput")
+    echo -ne "\r\033[0Krunning/total = $nrunning/$njobs"
     if [ $nrunning -eq 0 ]; then
         echo -e "\njobs finished\n"
         jobsrunning=false
+    else 
+        sleep 60 #wait a minute before trying again
     fi
 done
 rm $qstatoutput
