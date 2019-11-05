81780628cd833c5bf3bcbb9bd33bbd757b7ffe8c
bvermass <basilevermassen@hotmail.com>
2019-11-05 15:23:29 +0100

    New method to calculate upara and uperp. Some extra histograms. add samplelists for ee final state

--- src/MET_histograms.cc
+++ src/MET_histograms.cc
@@ -17,7 +17,11 @@ void full_analyzer::add_MET_histograms(std::map<TString, TH1*>* hists, TString p
     (*hists)[prefix+"_q_pt"]                    = new TH1F(prefix+"_q_pt", ";q #it{p}_{T} [GeV];Events", 100, 0, 200);
     (*hists)[prefix+"_uT_pt"]                   = new TH1F(prefix+"_uT_pt", ";u_{T} [GeV];Events", 100, 0, 200);
     (*hists)[prefix+"_upara"]                   = new TH1F(prefix+"_upara", ";u_{#parallel} + q_{T} [GeV];Events", 100, -200, 200);
-    (*hists)[prefix+"_uperp"]                   = new TH1F(prefix+"_uperp", ";u_{#perp} [GeV];Events", 100, -200, 200);
+    (*hists)[prefix+"_uperp"]                   = new TH1F(prefix+"_uperp", ";u_{#perp}  [GeV];Events", 100, -200, 200);
+    (*hists)[prefix+"_upara_frommetRaw"]        = new TH1F(prefix+"_upara_frommetRaw", ";u_{#parallel} + q_{T} [GeV] (Raw MET);Events", 100, -200, 200);
+    (*hists)[prefix+"_uperp_frommetRaw"]        = new TH1F(prefix+"_uperp_frommetRaw", ";u_{#perp}  [GeV] (Raw MET);Events", 100, -200, 200);
+
+    (*hists)[prefix+"_AbsScale"]                = new TH1F(prefix+"_AbsScale", ";-<u_{#parallel}> / <q_{T}>;Events", 100, -200, 200);
 }
 
 void full_analyzer::fill_MET_histograms(std::map<TString, TH1*>* hists, TString prefix, int i_leading, int i_subleading){
@@ -27,7 +31,7 @@ void full_analyzer::fill_MET_histograms(std::map<TString, TH1*>* hists, TString 
     (*hists)[prefix+"_metPhi"]->Fill(_metPhi, event_weight);
     (*hists)[prefix+"_metSMPhi"]->Fill(_metPhi, event_weight);//FIX
     (*hists)[prefix+"_metRawPhi"]->Fill(_metRawPhi, event_weight);
-   
+
     (*hists)[prefix+"_nPV"]->Fill(_nVertex, event_weight);
 
     // LorentzVector calculations
@@ -35,19 +39,21 @@ void full_analyzer::fill_MET_histograms(std::map<TString, TH1*>* hists, TString 
     LorentzVector l2_vec(_lPt[i_subleading], _lEta[i_subleading], _lPhi[i_subleading], _lE[i_subleading]);
     LorentzVector q_vec = l1_vec + l2_vec; // q_vec = Z boson
     LorentzVector met_vec(_met, 0, _metPhi, _met); // met is always transverse (eta = 0)
+    LorentzVector metRaw_vec(_metRaw, 0, _metRawPhi, _metRaw);
     LorentzVector uT_vec = -(q_vec + met_vec);  // Hadronic recoil u_T is defined as the sum of all PF particles momenta except for the Z boson. Therefore uT + q + met = 0
+    LorentzVector uT_frommetRaw_vec = -(q_vec + metRaw_vec);
 
-    double q_norm = sqrt(q_vec.px()*q_vec.px() + q_vec.py()*q_vec.py() + q_vec.pz()*q_vec.pz());
-    LorentzVector q_unitvec;
-    q_unitvec.setCartesianCoords( q_vec.px()/q_norm, q_vec.py()/q_norm, q_vec.pz()/q_norm, q_vec.energy()/q_norm );
-
-    double uT_dot_q_unit = q_unitvec.px()*uT_vec.px() + q_unitvec.py()*uT_vec.py() + q_unitvec.pz()*uT_vec.pz();
-    LorentzVector uT_para_vec;
-    uT_para_vec.setCartesianCoords(uT_dot_q_unit*q_unitvec.px(), uT_dot_q_unit*q_unitvec.py(), uT_dot_q_unit*q_unitvec.pz(), uT_dot_q_unit*q_unitvec.energy());
-    LorentzVector uT_perp_vec = uT_vec - uT_para_vec;
+    // Calculate, in TRANSVERSE PLANE, the parallel and perpendicular component of uT to Z
+    double upara = uT_vec.pt() * cos( uT_vec.phi() - q_vec.phi() );
+    double uperp = uT_vec.pt() * sin( uT_vec.phi() - q_vec.phi() );
+    double upara_frommetRaw = uT_frommetRaw_vec.pt() * cos( uT_frommetRaw_vec.phi() - q_vec.phi() );
+    double uperp_frommetRaw = uT_frommetRaw_vec.pt() * sin( uT_frommetRaw_vec.phi() - q_vec.phi() );
 
     (*hists)[prefix+"_q_pt"]->Fill(q_vec.pt(), event_weight);
     (*hists)[prefix+"_uT_pt"]->Fill(uT_vec.pt(), event_weight);
-    (*hists)[prefix+"_upara"]->Fill((uT_para_vec + q_vec).pt(), event_weight);
-    (*hists)[prefix+"_uperp"]->Fill(uT_perp_vec.pt(), event_weight);
+    (*hists)[prefix+"_upara"]->Fill(upara, event_weight);
+    (*hists)[prefix+"_uperp"]->Fill(uperp, event_weight);
+    (*hists)[prefix+"_upara_frommetRaw"]->Fill(upara_frommetRaw, event_weight);
+    (*hists)[prefix+"_uperp_frommetRaw"]->Fill(uperp_frommetRaw, event_weight);
+    (*hists)[prefix+"_AbsScale"]->Fill(-upara/q_vec.pt(), event_weight);
 }
