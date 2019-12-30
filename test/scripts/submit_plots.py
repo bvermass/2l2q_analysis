d946356f73e186f1efd2aefcc1844fbe7555a361
bvermass <basilevermassen@hotmail.com>
2019-12-30 19:10:20 +0100

    rename run_plots_new and delete old obsolete run_plots. Add some flexibility to submit_plots.py script

--- test/scripts/submit_plots.py
+++ test/scripts/submit_plots.py
@@ -15,15 +15,13 @@ def submit_script( script, scriptname ):
     while os.system('qsub {} -l walltime=20:00:00'.format(scriptname)) != 0:
         print 'qsub error caught, resubmitting'
         time.sleep(2)
+    os.system('rm {}'.format(scriptname))
 
-if len(sys.argv) != 1:
-    print('Wrong number of arguments, input should be: python test/scripts/submit_plots.sh')
+if len(sys.argv) != 3:
+    print('Wrong number of arguments, input should be: python test/scripts/submit_plots.sh [sampleList] [plot option]')
     sys.exit()
 
-script_bkg_mu = init_script('plots_bkg_mu.sh')
-script_bkg_mu.write('./test/run_plots_new.sh test/sampleLists/plots_bkg_mu.txt plots_bkg_mu.out 3')
-submit_script(script_bkg_mu, 'plots_bkg_mu.sh')
-
-#script_bkg_e = init_script('plots_bkg_e.sh')
-#script_bkg_e.write('./test/run_plots_new.sh test/sampleLists/plots_bkg_e.txt plots_bkg_e.out 3')
-#submit_script(script_bkg_e, 'plots_bkg_e.sh')
+scriptname = sys.argv[1].replace('.txt', '.sh')
+script_bkg_mu = init_script(scriptname)
+script_bkg_mu.write('./test/run_plots.sh test/sampleLists/{} submit_plots.out {}'.format(sys.argv[1], sys.argv[2]))
+submit_script(script_bkg_mu, scriptname)
