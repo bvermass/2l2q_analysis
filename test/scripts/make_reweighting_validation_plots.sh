c9c9b36f22ebc9501a91dbb6464bc8f01aaa9066
bvermass <basilevermassen@hotmail.com>
2020-02-19 11:43:40 +0100

    Add sampleLists for Electron data, for all sample submission of full_analyzer, for reweighting validation. Remove ZG and WG that are apparently fully included in WJets and DY respectively.

---
+++ test/scripts/make_reweighting_validation_plots.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL2e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL3e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL4e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL5e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL6e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL8e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL10e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL15e.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL2mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL3mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL4mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL5mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL6mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL8mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL10mu.txt 2 jlef.out
+./test/run_plots.sh test/sampleLists/multihists/plots_HNL15mu.txt 2 jlef.out
