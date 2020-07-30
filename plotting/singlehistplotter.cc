180984b23e717940cfa9ba8178994ddabdb1a712
bvermass <basilevermassen@hotmail.com>
2020-07-30 16:35:37 +0200

    Extra plots of obs. and pred. curves together for checking closure. Also extra plots that show signal eff. on top of ABCD prediction.

--- plotting/singlehistplotter.cc
+++ plotting/singlehistplotter.cc
@@ -123,7 +123,6 @@ int main(int argc, char * argv[])
                 if(histname.Contains("Shape_SR")) shapeSR_text->Draw(histname);
 
                 pad->Modified();
-                gSystem->Exec("pwd " + pathname_lin);
                 c->Print(pathname_lin + histname + ".png");
 
                 // Efficiencies are calculated right here as TGraphAsymmErrors
@@ -153,13 +152,15 @@ int main(int argc, char * argv[])
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_quadAB_", "_quadCD_"}, {"Region A+B", "Region C+D"}, "_quadAB-CD_", true);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_quadAC_", "_quadBD_"}, {"Region A+C", "Region B+D"}, "_quadAC-BD_", true);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_DtoCwithCD_", "_quadC_"}, {"Region C (pred.)", "Region C"}, "_quadCwithDtoC_", false);
-                    plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_BtoAwithCD_", "_quadA_"}, {"pred.", "obs."}, "_quadAwithBtoA_", false);
+                    plot_2_hists_with_ratio(sample_file, general_pathname, sample_hist, histname, colors, is2016, is2017, is2018, {"_BtoAwithCD_", "_quadA_"}, {"pred.", "obs."}, "_quadAwithBtoA_", false);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_quadA_", "_quadB_"}, {"Region A", "Region B"}, "_quadA-B_", true);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_quadA_", "_quadC_"}, {"Region A", "Region C"}, "_quadA-C_", true);
                     if(isData) plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_quadC_", "_quadD_"}, {"Region C", "Region D"}, "_quadC-D_", true);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_AoverB_", "_CoverD_"}, {"A/B", "C/D"}, "_AoverB-CoverD_", false);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_cutphill_", "_cutmll_", "_cutphiORmll_", "_cutmlSV_"}, {"#Delta #phi", "m_{ll}", "#Delta #phi or m_{ll}", "m_{l,SV}"}, "_cutAll_", false);
                     plot_normalized_hists(sample_file, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text, {"_cutTightphill_", "_cutTightmll_", "_cutTightphiORmll_", "_cutTightmlSV_"}, {"#Delta #phi", "m_{ll}", "#Delta #phi or m_{ll}", "m_{l,SV}"}, "_cutTightAll_", false);
+
+                    if(isData and histname.Contains("Shape_SR") and histname.Contains("BtoAwithCD") and !histname.Contains("M-15")) plot_Shape_SR_with_Signal_eff(inputfilename, general_pathname, sample_hist, histname, c, pad, legend, colors, CMSandLumi, shapeSR_text);
                 }
             }else if(cl->InheritsFrom("TH2")){
                 TH2F *sample_hist = (TH2F*)key->ReadObj();
@@ -194,6 +195,95 @@ int main(int argc, char * argv[])
     }
 }
 # endif
+
+void plot_2_hists_with_ratio(TFile* sample_file, TString general_pathname, TH1F* sample_hist, TString histname, std::vector<int> colors, bool is2016, bool is2017, bool is2018, std::vector<TString> tags, std::vector<TString> legend_tags, TString plot_tag, bool normalize_to_1)
+{
+    if(histname.Contains(tags[0]) and tags.size() == 2){
+        TCanvas c2("c2","",700,700);
+        c2.cd();
+
+        TPad pad_histo("pad_histo", "", 0., 0.33, 1., 1.);
+        pad_histo.SetTopMargin(0.07);
+        pad_histo.SetBottomMargin(0.01);
+        pad_histo.Draw();
+        pad_histo.cd();
+        float leftmargin  = pad_histo.GetLeftMargin();
+        float topmargin   = pad_histo.GetTopMargin();
+        float rightmargin = pad_histo.GetRightMargin();
+
+        CMSandLuminosity CMSandLumi(&pad_histo, is2016, is2017, is2018);
+        Shape_SR_plottext shapeSR_text(&pad_histo);
+        TLegend legend = get_legend(0.2, 0.84, 0.95, 0.9, 4);
+
+        c2.cd();
+        TPad pad_ratio("pad_ratio", "", 0., 0., 1., 0.32);
+        RatioPlot ratioplotter(&pad_ratio);
+
+        legend.Clear();
+
+        THStack* hists = new THStack("stack", "");
+
+        hists->Add(sample_hist);
+        if(normalize_to_1) sample_hist->Scale(1./sample_hist->Integral());
+        sample_hist->SetMarkerColor(colors[0]);
+        sample_hist->SetLineColor(colors[0]);
+        legend.AddEntry(sample_hist, legend_tags[0], "pl");
+
+        TString histname_extra = histname;
+        histname_extra.ReplaceAll(tags[0], tags[1]);
+        TH1F* hist_extra = (TH1F*)sample_file->Get(histname_extra);
+        if(!hist_extra){
+            std::cout << "hist " << histname_extra << " does not exist" << std::endl;
+            return;
+        }
+        hist_extra->SetMarkerColor(colors[1]);
+        hist_extra->SetLineColor(colors[1]);
+        hists->Add(hist_extra);
+        legend.AddEntry(hist_extra, legend_tags[1], "pl");
+        if(normalize_to_1) hist_extra->Scale(1./hist_extra->Integral());
+
+        ratioplotter.SetCentralRatio(hist_extra, sample_hist, sample_hist->GetXaxis()->GetTitle(), "obs/pred");
+
+        TString plotname = histname;
+        plotname.ReplaceAll(tags[0], plot_tag);
+        TString pathname_lin    = make_plotspecific_pathname(plotname, general_pathname, "lin/");
+        TString pathname_log    = make_plotspecific_pathname(plotname, general_pathname, "log/");
+
+        pad_histo.Clear();
+        pad_histo.SetLogy(0);
+
+        hists->Draw("E P hist nostack");
+        hists->GetXaxis()->SetTitle(sample_hist->GetXaxis()->GetTitle());
+        hists->GetYaxis()->SetTitle(sample_hist->GetYaxis()->GetTitle());
+        hists->SetMaximum(1.25*hists->GetMaximum("nostack"));
+        if(plot_tag.Contains("AoverB")) hists->SetMaximum(std::min((1.25*hists->GetMaximum("nostack")), 10.));
+        hists->SetMinimum(0);
+        legend.Draw("same");
+        CMSandLumi.Draw();
+        if(histname.Contains("Shape_SR")) shapeSR_text.Draw(histname);
+
+        pad_histo.Modified();
+        ratioplotter.dothething();
+        c2.Print(pathname_lin + plotname + ".png");
+
+        // Draw log version
+        //pad_histo.Clear();
+        //pad_histo.SetLogy(1);
+
+        //hists->Draw("E P hist nostack");
+        //hists->GetXaxis()->SetTitle(sample_hist->GetXaxis()->GetTitle());
+        //hists->GetYaxis()->SetTitle(sample_hist->GetYaxis()->GetTitle());
+        //hists->SetMaximum(10*hists->GetMaximum("nostack"));
+        //hists->SetMinimum(std::max(1e-6, 0.5*hists->GetMinimum("nostack")));
+        //legend.Draw("same");
+        //CMSandLumi.Draw();
+        //if(histname.Contains("Shape_SR")) shapeSR_text.Draw(histname);
+
+        //pad_histo.Modified();
+        //ratioplotter.dothething();
+        //c2.Print(pathname_log + plotname + ".png");
+    }
+}
 
 void plot_normalized_hists(TFile* sample_file, TString general_pathname, TH1F* sample_hist, TString histname, TCanvas* c, TPad* pad, TLegend legend, std::vector<int> colors, CMSandLuminosity* CMSandLumi, Shape_SR_plottext* shapeSR_text, std::vector<TString> tags, std::vector<TString> legend_tags, TString plot_tag, bool normalize_to_1)
 {
@@ -252,9 +342,108 @@ void plot_normalized_hists(TFile* sample_file, TString general_pathname, TH1F* s
         //hists->SetMinimum(std::max(1e-6, 0.5*hists->GetMinimum("nostack")));
         //legend.Draw("same");
         //CMSandLumi->Draw();
-        //if(histname.Contains("Shape_SR") and histname.Contains("_2l")) shapeSR_text->Draw();
+        //if(histname.Contains("Shape_SR")) shapeSR_text->Draw(histname);
 
         //pad->Modified();
         //c->Print(pathname_log + plotname + ".png");
     }
+}
+
+void plot_Shape_SR_with_Signal_eff(TString filename, TString general_pathname, TH1F* sample_hist, TString histname, TCanvas* c, TPad* pad, TLegend legend, std::vector<int> colors, CMSandLuminosity* CMSandLumi, Shape_SR_plottext* shapeSR_text)
+{
+    if(!histname.Contains("M-") or !histname.Contains("V2-")) return;
+
+    pad->Clear();
+    pad->SetLogy(0);
+    legend.Clear();
+    TString pathname_lin    = make_plotspecific_pathname(histname, general_pathname, "lin/");
+
+    sample_hist->SetMarkerColor(kBlack);
+    sample_hist->SetLineColor(kBlack);
+
+    //get corresponding signal
+    TString mass = (TString)histname(histname.Index("_M-"), histname.Index("_V2-") - histname.Index("_M-"));
+    TString flavor;
+    if(histname.Contains("_mm")) flavor = "_mu";
+    if(histname.Contains("_ee")) flavor = "_e";
+    if(histname.Contains("_2l") or histname.Contains("_em") or histname.Contains("_me")) flavor = "_2l";
+
+    TString year;
+    if(filename.Contains("2016")) year = "_MiniAOD2016";
+    if(filename.Contains("2017")) year = "_MiniAOD2017";
+    if(filename.Contains("2018")) year = "_MiniAOD2018";
+
+    TString full_HNL_name = (TString)filename(0, filename.Index("_analyzer_") + 10) + "HeavyNeutrino_lljj" + mass + "_V-combined" + flavor + "_massiveAndCKM_LO" + year + ".root";
+    TFile*  HNL_file   = TFile::Open(full_HNL_name);
+
+    TString histname_quadA = histname;
+    histname_quadA.ReplaceAll("BtoAwithCD", "quadA");
+    TH1F* HNL_hist_quadA = (TH1F*)HNL_file->Get(histname_quadA);
+    HNL_hist_quadA->SetMarkerColor(colors[0]);
+    HNL_hist_quadA->SetLineColor(colors[0]);
+
+    //getting quadB, C and D as well and calculating percent of events in A
+    histname_quadA.ReplaceAll("quadA", "quadB");
+    TH1F* HNL_hist_quadB = (TH1F*)HNL_file->Get(histname_quadA);
+    HNL_hist_quadB->SetMarkerColor(colors[2]);
+    HNL_hist_quadB->SetLineColor(colors[2]);
+    histname_quadA.ReplaceAll("quadB", "quadC");
+    TH1F* HNL_hist_quadC = (TH1F*)HNL_file->Get(histname_quadA);
+    HNL_hist_quadC->SetMarkerColor(colors[3]);
+    HNL_hist_quadC->SetLineColor(colors[3]);
+    histname_quadA.ReplaceAll("quadC", "quadD");
+    TH1F* HNL_hist_quadD = (TH1F*)HNL_file->Get(histname_quadA);
+    HNL_hist_quadD->SetMarkerColor(colors[4]);
+    HNL_hist_quadD->SetLineColor(colors[4]);
+
+    TH1F* HNL_hist_effA = (TH1F*)HNL_hist_quadA->Clone();
+    HNL_hist_effA->Add(HNL_hist_quadB);
+    HNL_hist_effA->Add(HNL_hist_quadC);
+    HNL_hist_effA->Add(HNL_hist_quadD);
+
+    HNL_hist_effA->Divide(HNL_hist_quadA, HNL_hist_effA);
+    HNL_hist_effA->SetMarkerColor(colors[1]);
+    HNL_hist_effA->SetLineColor(colors[1]);
+    HNL_hist_effA->GetYaxis()->SetRangeUser(0, 1.1);
+
+    // Make the pad that will contain the plot
+    c->cd();
+    TPad* pad_eff  = new TPad("pad_eff", "", 0., 0., 1., 1.);
+    pad_eff->Draw();
+    pad_eff->cd();
+    pad_eff->SetFillStyle(4000);
+    pad_eff->SetFrameFillStyle(0);
+    pad_eff->SetTicky(0);
+    pad_eff->SetRightMargin(0.07);
+
+    HNL_hist_effA->Draw("E P Y+");
+
+    pad->cd();
+    pad->SetTicky(0);
+    pad->SetRightMargin(0.07);
+    THStack* hists = new THStack("stack", "");
+    hists->Add(sample_hist);
+    hists->Add(HNL_hist_quadA);
+    hists->Add(HNL_hist_quadB);
+    hists->Add(HNL_hist_quadC);
+    hists->Add(HNL_hist_quadD);
+
+
+    legend.AddEntry(sample_hist, "Data pred.", "pl");
+    legend.AddEntry(HNL_hist_quadA, adjust_legend("HNL" + mass), "pl");
+    legend.AddEntry(HNL_hist_effA, "signal eff.", "pl");
+    legend.AddEntry(HNL_hist_quadB, "quad B", "pl");
+    legend.AddEntry(HNL_hist_quadC, "quad C", "pl");
+    legend.AddEntry(HNL_hist_quadD, "quad D", "pl");
+
+    hists->Draw("E P hist nostack");
+    legend.Draw("same");
+    CMSandLumi->Draw();
+    if(histname.Contains("Shape_SR")) shapeSR_text->Draw(histname);
+
+    pad->Modified();
+    c->Print(pathname_lin + histname + "_withHNL.png");
+    pad->SetTicky(1);
+    pad->SetRightMargin(0.04);
+    pad_eff->Clear();
 }
--- plotting/singlehistplotter.h
+++ plotting/singlehistplotter.h
@@ -22,11 +22,14 @@
 // other header files
 #include "tdrStyle.h"
 #include "helper_plotter_functions.h"
+#include "ratioplotter.h"
 #include "../interface/helper_histo_functions.h"
 
 # ifndef __CINT__ 
 int main(int argc, char * argv[]);
 #endif
+void plot_2_hists_with_ratio(TFile* sample_file, TString general_pathname, TH1F* sample_hist, TString histname, std::vector<int> colors, bool is2016, bool is2017, bool is2018, std::vector<TString> tags, std::vector<TString> legend_tags, TString plot_tag, bool normalize_to_1);
 void plot_normalized_hists(TFile* sample_file, TString general_pathname, TH1F* sample_hist, TString histname, TCanvas* c, TPad* pad, TLegend legend, std::vector<int> colors, CMSandLuminosity* CMSandLumi, Shape_SR_plottext* shapeSR_text, std::vector<TString> tags, std::vector<TString> legend_tags, TString plot_tag, bool normalize_to_1);
+void plot_Shape_SR_with_Signal_eff(TString filename, TString general_pathname, TH1F* sample_hist, TString histname, TCanvas* c, TPad* pad, TLegend legend, std::vector<int> colors, CMSandLuminosity* CMSandLumi, Shape_SR_plottext* shapeSR_text);
 
 #endif
