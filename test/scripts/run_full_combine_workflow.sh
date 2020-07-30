6d30af9292d27e895351edfecc9c9f3362ec45fc
bvermass <basilevermassen@hotmail.com>
2020-07-30 17:07:54 +0200

    scripts for full combine limit setting workflow

--- test/scripts/run_full_combine_workflow.sh
+++ test/scripts/run_full_combine_workflow.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
 ./test/scripts/run_combine_datacards_all.sh
-cd ~/heavyNeutrino/Dileptonprompt/CMSSW_8_1_0/src/CombineScripts/
-eval `scram runtime -sh`
-./setup.sh
-./moveScriptToDirAndRunCombine.sh
-cd ~/heavyNeutrino/Dileptonprompt/CMSSW_10_2_14/src/2l2q_analysis/helpertools/CombineTools
-cmsenv
+./test/scripts/wait_until_jobs_are_finished.sh
+cd helpertools/CombineTools/
+python runCombine.py 5
+./../../test/scripts/wait_until_jobs_are_finished.sh
 ./runCombineOutputPrep.sh
 ./runAnalyzeCombineOutput.sh
